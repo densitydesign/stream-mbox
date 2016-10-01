97cf57492efb03ca518d8d45373c96279fca2dd8
iosonosempreio <tommaso.elli@gmail.com>
2016-10-01 18:06:14 +0200

    saves attachments

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # Node.js .mbox parser
+This script accepts a `.mbox` archive as input and parses it returning the following:
+- data wrapped into a CSV file:
+	- date
+	- from
+	- to
+	- cc
+	- bcc
+	- subject
+	- gmail_label
+	- attachments
+- a folder where it saves messages attachments
+	- data/attachments-my-messages.mbox-mailbox
 
 ### How to use
 0. Install [node.js](https://nodejs.org) and [Github Desktop](https://desktop.github.com)
 1. Clone the repo: click on the green button **Clone or Download** > **Open in Desktop**
-2. Open the terminal and navigate to the repo: `cd path/to/repo/`
+2. Open the terminal and navigate to the repo folder: `cd path/to/repo/`
 3. Install dependencies: `npm install`
-3. create `data` folder: `mkdir data`
-4. Place your `.mbox` file into the `data` folder
+3. create a folder and name it **data**: `mkdir data`
+4. Place your **.mbox** file into the **data** folder
+3. Open the file called **index.js** and change the variable on first line (`var nameMbox = 'Spam.mbox';`) with the name of your **.mbox** file
 6. Run: `node index.js`
 
 ### Credits
--- index.js
+++ index.js
@@ -1,33 +1,65 @@
-var nameMbox = 'Spam';
+var nameMbox = 'Spam.mbox';
 
 var fs = require('fs')
 var MailParser  = require('mailparser').MailParser;
 var Mbox        = require('node-mbox');
-var mbox        = new Mbox('data/'+nameMbox+'.mbox');
+var mbox        = new Mbox('data/'+nameMbox);
 
-var header = 'date\tfrom\tto\tcc\tbcc\tsubject\tgmail_label\n'
+var header = 'date\tfrom\tto\tcc\tbcc\tsubject\tgmail_label\tattachments\n'
 
 fs.writeFileSync('data/parsed-'+nameMbox+'.tsv', header);
 
-// wait for message events
+// Wait for "message events"
 mbox.on('message', function(msg) {
-  
-  var tsv = '';
+    
+    var tsv = '';
 
-  // parse message using MailParser
-  var mailparser = new MailParser({ streamAttachments : true });
-  mailparser.on('headers', function(headers) {
+    // parse message using MailParser
+    var mailparser = new MailParser({ streamAttachments : true });
 
-    tsv = headers.date+'\t'+headers.from+'\t'+headers.to+'\t'+headers.cc+'\t'+headers.bcc+'\t'+headers.subject+'\t'+headers['x-gmail-labels']+'\n'
+    // One the hedaers have been detected, get basic information
+    mailparser.on('headers', function(headers) {
+        tsv += headers.date+'\t'+headers.from+'\t'+headers.to+'\t'+headers.cc+'\t'+headers.bcc+'\t'+headers.subject+'\t'+headers['x-gmail-labels']
+    });
 
-    fs.appendFileSync('data/parsed-'+nameMbox+'.tsv', tsv)
+    // Save attached files
+    mailparser.on("attachment", function(attachment, mail){
+        var folder = 'data/attachments-'+nameMbox+'-mailbox/'
+        if (!fs.existsSync(folder)){
+            fs.mkdirSync(folder);
+        }
+        var output = fs.createWriteStream(folder + attachment.generatedFileName);
+        attachment.stream.pipe(output);
+    });
 
-  });
+    // Once the whole message has been read, extract the attachment file names
+    mailparser.on("end", function(mail_object){
+        var attachmentsData = '';
+        if (mail_object.attachments) {
+            mail_object.attachments.forEach(function(attachment,i){
+                // Put a comma after name, only if this is not the last element.
+                // Grab the attachment names into quotation marks to get a sturdier dataset
+                if (i < mail_object.attachments.length-1) {
+                    attachmentsData += '"'+attachment.fileName+'", '
+                } else {
+                    attachmentsData += '"'+attachment.fileName+'"'
+                }  
+            })
+        }    
+        tsv += '\t'+attachmentsData
 
-  mailparser.write(msg);
-  mailparser.end();
+        // Close the line before appending
+        tsv += '\n'
+        // Append line to dataset
+        fs.appendFileSync('data/parsed-'+nameMbox+'.tsv', tsv)
+        
+    });
+
+    mailparser.write(msg);
+    mailparser.end();
+
 });
 
 mbox.on('end', function() {
-  console.log('Done. Go to ./data/parsed-'+nameMbox+'.tsv');
+    console.log('Done. Go to data/parsed-'+nameMbox+'.tsv');
 });
